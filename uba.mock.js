7568a629503b8ea7f2867bfaef05683cab4523d6
kvkens <kvkens@qq.com>
2017-06-07 13:05:24 +0800

    add:增加mock配置路由文件，对应uba-server更新.

---
+++ uba.mock.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "GET": [{
+    "/User/Home": "./mock/api/user/home.json"
+  }, {
+    "/User/Get": "./mock/api/user/allsection.json"
+  }],
+  "POST": [{
+    "/User/Post": "./mock/api/user/home.json"
+  },{
+    "/User/Send": "./mock/api/send/user.json"
+  }]
+}
